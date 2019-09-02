1da5bbf2d6ecca25d38faff7a114c4eda9a08b79
muskan-1527 <agrawalm8832gmail.com>
2019-09-02 19:59:52 +0530

    css mosule , media queries , pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
-import Radium, {StyleRoot} from 'radium';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component{
@@ -47,20 +46,8 @@ class App extends Component{
   
  render() {
 
-  const style = {
-    backgroundColor:'green',
-    color: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding:'8px',
-    cursor: 'pointer',
-    ':hover' : {
-      backgroundColor: 'lightgreen',
-      color: 'black'
-    }
-  }
-
   let persons=null;
+  let btnClass = '';
   if(this.state.showPersons){
     persons = (
      
@@ -74,36 +61,35 @@ class App extends Component{
       key={person.id}
       changed= {(event) => this.nameChangedHandler(event, person.id)}/>
     })}
-    </div> );
-    style.backgroundColor = 'red';
-    style[':hover'] = {
-      backgroundColor: 'salmon',
-      color: 'black'
-    }
+    </div>
+     );
+
+     btnClass = classes.Red;
+
   }
 
-  const classes = [];
+  const assignedClasses = [];
   if(this.state.persons.length <= 2) {
-    classes.push('red');
+    assignedClasses.push(classes.red);
   }
 
   if(this.state.persons.length <= 1) {
-    classes.push('bold');
+    assignedClasses.push(classes.bold);
   }
 
   return (
-    <StyleRoot>
-    <div className="App">
+
+    <div className={classes.App}>
      <h1>Hi, I'm a React App</h1>
-     <p className={classes.join(' ')}>This is really working!</p>
+     <p className={assignedClasses.join(' ')}>This is really working!</p>
      <button
-     style={style}
+     className={btnClass}
      onClick={this.togglePersonsHandler}>Toggle Persons</button>
     {persons}
     </div>
-    </StyleRoot>
+
   );
 }
 }
-export default Radium(App);
+export default App;
 
